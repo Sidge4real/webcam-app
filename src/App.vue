61d9f569e5f2ee2da339af4b386f7d85d62ea530
Lukas Van der Spiegel <lukas.vanderspiegel@external.atlascopco.com>
2023-08-18 08:58:38 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <video ref="videoElement" autoplay></video>
+    <button @click="takePhoto" :disabled="photoList.length >= 2">Take Photo</button>
+    <div class="photo-list">
+      <img
+        v-for="(photoDataUrl, index) in photoList"
+        :key="index"
+        :src="photoDataUrl"
+        alt="Taken Photo"
+        @click="showFullscreen(index)"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const videoElement = ref(null);
+    const photoList = ref([]);
+
+    const startCamera = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        const selectedDevice = videoDevices[0];
+
+        const constraints = { video: { deviceId: selectedDevice.deviceId } };
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        videoElement.value.srcObject = stream;
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    };
+
+    const takePhoto = () => {
+      if (photoList.value.length < 2) {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = videoElement.value.videoWidth;
+        canvas.height = videoElement.value.videoHeight;
+        context.drawImage(videoElement.value, 0, 0, canvas.width, canvas.height);
+        const dataUrl = canvas.toDataURL('image/jpeg');
+        photoList.value.push(dataUrl);
+      }
+    };
+
+    const showFullscreen = index => {
+      const img = new Image();
+      img.src = photoList.value[index];
+      const viewer = window.open('', '_blank');
+      viewer.document.write(img.outerHTML);
+    };
+
+    onMounted(() => {
+      startCamera();
+    });
+
+    return {
+      videoElement,
+      photoList,
+      takePhoto,
+      showFullscreen,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app-container {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+video {
+  width: 50%;
+  height: 50%;
+}
+.photo-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+.photo-list img {
+  max-width: 100px;
+  height: auto;
+  margin: 5px;
+  cursor: pointer;
 }
 </style>
